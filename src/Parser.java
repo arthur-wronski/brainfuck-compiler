0e0184aa509380d957e6e11f62112782f0d5a16e
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-11-26 21:05:10 +0000

    [FEAT] implemented parser

---
+++ src/Parser.java
@@ -0,0 +1,44 @@
+import java.util.Queue;
+
+// here's how you construct valid code in Brainfuck:
+// alphabet: Σ := > | < | + | - | . | , | [ | ]
+// Operator: Ω:= > | < | + | - | . | ,
+// Valid Code: Code := [Code] | Ω*
+
+public class Parser {
+    public void parseTokenQueue(Queue<Token> tokenQueue) throws Exception{
+        // if queue not empty -> error in parsing throw exception
+        outerParser(tokenQueue);
+        if (!tokenQueue.isEmpty()){
+            throw new Exception("Error: error whilst parsing");
+        }
+        else{
+            System.out.println("I cooked");
+        }
+    }
+
+    void outerParser(Queue<Token> tokenQueue) throws Exception{
+        while (!tokenQueue.isEmpty()) {
+            Token token = tokenQueue.peek();
+
+            if (token == Token.JUMP_LEFT){
+                return;
+            } else if (token == Token.JUMP_RIGHT) {
+                tokenQueue.poll();
+                loopParser(tokenQueue);
+            }else {
+                tokenQueue.poll();
+            }
+
+        }
+        // queue being empty is fine
+    }
+
+    private void loopParser(Queue<Token> tokenQueue) throws Exception {
+        outerParser(tokenQueue);
+
+        if (tokenQueue.poll() != Token.JUMP_LEFT){
+            throw new Exception("Error");
+        }
+    }
+}
