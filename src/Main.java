ab7f4ff73fbc316cf58d1c284618605ee798b80f
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-11-26 22:03:14 +0000

    [TEST] print A
    successful somehow vamos

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Lexer lexer = new Lexer();
-        lexer.tokenizeCode("+++[>+++[>++<-]");
+        lexer.tokenizeCode("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
+
+        Queue<Token> instructionQueue = new ArrayDeque<Token>(lexer.getTokenQueue());
 
         Parser parser = new Parser();
         parser.parseTokenQueue(lexer.getTokenQueue());
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.executeCode(instructionQueue);
     }
 }
