2c59d0ad78c13c6ba9b35fa68976ad8195841ff8
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-11-27 12:51:49 +0000

    [REFACTOR] moved logic out of main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,8 @@
 package main;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Lexer lexer = new Lexer();
-//        lexer.tokenizeCode("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
-        lexer.tokenizeCode("[[[+++]]");
-
-        Queue<Token> instructionQueue = new ArrayDeque<Token>(lexer.getTokenQueue());
-
-        Parser parser = new Parser();
-        parser.parseTokenQueue(lexer.getTokenQueue());
-
-//        main.Interpreter interpreter = new main.Interpreter();
-//        interpreter.executeCode(instructionQueue);
+        Interpreter interpreter = new Interpreter();
+        interpreter.run("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.\n");
     }
 }
