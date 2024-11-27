b9077dcdeda5269d76f7bc4080607188b239b8f1
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-11-27 12:51:28 +0000

    [FEAT] implemented loop logic

--- src/main/Interpreter.java
+++ src/main/Interpreter.java
@@ -1,26 +1,67 @@
 package main;
 
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class Interpreter {
     private final TuringMachine turingMachine;
+    private List<Token> instructionList;
+    private int instructionPointer;
+    private final Map<Integer,Integer> loopIndexes;
 
     public Interpreter(){
         this.turingMachine = new TuringMachine();
+        this.instructionList = new ArrayList<>();
+        this.instructionPointer = 0;
+        this.loopIndexes = new HashMap<Integer, Integer>();
     }
 
-    public void executeCode(Queue<Token> instructionQueue){
-        while (!instructionQueue.isEmpty()){
-            Token instruction = instructionQueue.peek();
+    private void setInstructionList(List<Token> instructionList){
+        this.instructionList = instructionList;
+    }
+
+    private void setLoopIndexes(){
+        // keep indexes in stack
+        Stack<Integer> loopStartIndexes = new Stack<>();
+
+        for (int idx = 0; idx< instructionList.size(); idx++){
+            if (instructionList.get(idx) == Token.JUMP_RIGHT){
+                loopStartIndexes.push(idx);
+            } else if (instructionList.get(idx) == Token.JUMP_LEFT) {
+                int start = loopStartIndexes.pop();
+                loopIndexes.put(start, idx);
+                loopIndexes.put(idx, start);
+            }
         }
     }
 
-    private void executeLoop(Queue<Token> instructionQueue){
+    public void run(String code) throws Exception {
+        Lexer lexer = new Lexer();
+        lexer.tokenizeCode(code);
 
+        List<Token> instructionList = new ArrayList<>(lexer.getTokenQueue());
+
+        Parser parser = new Parser();
+        parser.parseTokenQueue(lexer.getTokenQueue());
+
+        setInstructionList(instructionList);
+        setLoopIndexes();
+        executeCode();
+    }
+
+    private void executeCode(){
+        while (instructionPointer < instructionList.size()){
+            Token instruction = instructionList.get(instructionPointer);
+            executeInstruction(instruction);
+            instructionPointer++;
+        }
     }
 
     private void executeInstruction(Token instruction){
-        switch (instruction){
+        switch (instruction) {
             case Token.MOVE_RIGHT:
                 turingMachine.moveHeadRight();
                 break;
@@ -41,10 +82,16 @@ public class Interpreter {
                 break;
             case Token.JUMP_RIGHT:
                 // if value at pointer is 0 -> move forward to instruction after corresponding ]
-                // otherwise nothing
+                if (turingMachine.getValueAtPointer() == 0) {
+                    instructionPointer = loopIndexes.get(instructionPointer);
+                }
+                break;
             case Token.JUMP_LEFT:
-                // if value at pointer is 0 -> move back to instruction after corresponding [
-                // otherwise nothing
+                // if value at pointer is not 0 -> move back to instruction after corresponding [
+                if (turingMachine.getValueAtPointer() != 0) {
+                    instructionPointer = loopIndexes.get(instructionPointer);
+                }
+                break;
         }
     }
 }
